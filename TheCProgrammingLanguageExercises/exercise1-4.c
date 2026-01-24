21b79fa02aadf33ed7a52eab6b0ca61d7501f461
Joanna Peniket <joannapeniket@hotmail.com>
2026-01-24 13:40:23 +0000

    Exercises 1.1 - 1.4 of Textbook

---
+++ TheCProgrammingLanguageExercises/exercise1-4.c
@@ -0,0 +1,21 @@
+/* Exercise 1.4 - Write a programto print the corresponding Celsius to Fahrenheit table */
+
+#include <stdio.h>
+
+int main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    printf("Conversion Chart (Celsius -> Fahrenheit)\n");
+    celsius = lower;
+    while (celsius <= upper) {
+        fahr = (9.0/5.0) * celsius + 32.0;
+        printf("%3.0f %6.1f\n" ,celsius, fahr);
+        celsius = celsius + step;
+    }
+}
